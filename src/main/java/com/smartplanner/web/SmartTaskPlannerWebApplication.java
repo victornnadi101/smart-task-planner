8bdd65780fa10682851e52e4565ae41a27551954
Victor Nnadi <victornnadi101@outlook.com>
2026-04-15 00:42:27 -0400

    update API to production URL

--- src/main/java/com/smartplanner/web/SmartTaskPlannerWebApplication.java
+++ src/main/java/com/smartplanner/web/SmartTaskPlannerWebApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartTaskPlannerWebApplication {
-
     public static void main(String[] args) {
         DatabaseSetup.initialize();
         SpringApplication.run(SmartTaskPlannerWebApplication.class, args);
